7ebd0c176b1386642cb20e2fd7fe628eced7c031
feltsbar <kuzmin.max6569@gmail.com>
2022-05-07 20:33:52 +0300

    Created theme for DictionaryActivity, extend activity_dictionary.xml

--- app/src/main/java/com/example/vocab/presentation/DictionaryActivity.kt
+++ app/src/main/java/com/example/vocab/presentation/DictionaryActivity.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.example.vocab.R
 
 class DictionaryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dictionary)
+
     }
 
     companion object {
