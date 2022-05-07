279f9855d8e8208337db8d90c89c7ab1aea40cbe
feltsbar <kuzmin.max6569@gmail.com>
2022-05-07 16:32:21 +0300

    Added plugin 'kotlin-android-extensions' for using synthetics in project

--- app/src/main/java/com/example/vocab/presentation/MainActivity.kt
+++ app/src/main/java/com/example/vocab/presentation/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.vocab.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.vocab.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        card_view_user_dictionary.setOnClickListener {
+
+        }
     }
 }
