3c7d181c47dcdac3a5de6362d235183019e610f1
feltsbar <kuzmin.max6569@gmail.com>
2022-05-19 19:45:57 +0300

    Added TranslateActivity, changed VocabTheme and dictionary_theme. Renamed dictionary_theme.xml to activity_main_theme.xml

---
+++ app/src/main/java/com/example/vocab/presentation/TranslateActivity.kt
@@ -0,0 +1,14 @@
+package com.example.vocab.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.vocab.R
+
+class TranslateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_translate)
+
+
+    }
+}
