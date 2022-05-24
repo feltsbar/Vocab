e6e87752f224e96376e10e118d27180e68c936ff
feltsbar <kuzmin.max6569@gmail.com>
2022-05-24 23:23:36 +0300

    All connected but data is not displayed in tv_translated_text

--- app/src/main/java/com/example/vocab/presentation/activities/TranslateActivity.kt
+++ app/src/main/java/com/example/vocab/presentation/activities/TranslateActivity.kt
@@ -4,50 +4,78 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.vocab.R
 import com.example.vocab.data.api.ApiFactory
 import com.example.vocab.data.pojo.PostBody
+import com.example.vocab.presentation.view_models.TranslateViewModel
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_translate.*
 
 class TranslateActivity : AppCompatActivity() {
 
     private val compositeDisposable = CompositeDisposable()
+    private val viewModel by lazy {
+        ViewModelProvider(
+            this,
+            defaultViewModelProviderFactory
+        )[TranslateViewModel::class.java]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_translate)
 
-        // todo вызвать метод requestToTranslator() и задать в параметры значения из разметки
+        // Переопределение функции срабатываемой при на Enter экранной клавиатуры
+        et_text_to_translate.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                tv_translated_text.text = requestToTranslator(
+                    viewModel.collectUserDataToPostBody(
+                        listOf(et_text_to_translate.text?.toString()),
+                        tv_target_language.text.toString(),
+                        tv_source_language.text.toString()
+                    )
+                )
+                return@OnKeyListener true
+            }
+            false
+        })
 
-
+        button_change_language.setOnClickListener {
+            changeTranslatingLanguage()
+        }
     }
 
-    private fun requestToTranslator(
-        inputTexts : List<String>,
-        targetLanguage : String,
-        sourceLanguage : String
-    ) : String {
+    // Метод меняет местами языки с какого на какой будет производиться перевод
+    private fun changeTranslatingLanguage() {
+        val mediatorLanguage = tv_source_language.text.toString()
+        tv_source_language.text = tv_target_language.text.toString()
+        tv_target_language.text = mediatorLanguage
+    }
 
-        var result = ""
-        val body = PostBody(
-            texts = inputTexts,
-            targetLanguageCode = targetLanguage,
-            sourceLanguageCode = sourceLanguage
-        )
-        val disposable = ApiFactory.apiService.getTranslatedText(body)
+    private fun requestToTranslator(postBody: PostBody): String {
+        var resultOfTranslate = ""
+        val disposable = ApiFactory.apiService.getTranslatedText(postBody)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
-                result = it.toString()
-                Log.d("LOG", it.toString())
+                resultOfTranslate = it.translationsList?.get(0)?.text.toString()
+                Log.d("LOG", resultOfTranslate)
             }, {
                 Log.d("LOG", it.message.toString())
             })
         compositeDisposable.addAll(disposable)
-        return result
+        return resultOfTranslate
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 
     companion object {
@@ -55,11 +83,6 @@ class TranslateActivity : AppCompatActivity() {
             val intent = Intent(context, TranslateActivity::class.java)
             return intent
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        compositeDisposable.dispose()
     }
 
 }
--- app/src/main/java/com/example/vocab/presentation/view_models/TranslateViewModel.kt
+++ app/src/main/java/com/example/vocab/presentation/view_models/TranslateViewModel.kt
@@ -2,7 +2,47 @@ package com.example.vocab.presentation.view_models
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.example.vocab.data.DictionaryRepositoryImpl
+import com.example.vocab.data.pojo.PostBody
+import com.example.vocab.domain.use_cases.AddUserWordUseCase
 
 class TranslateViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository = DictionaryRepositoryImpl(application)
+    private val addUserWordUseCase = AddUserWordUseCase(repository)
 
+
+    // Метод собирает введенный пользователем текст и проверяет языки с какого на какой переводить
+    // и сам введеный текст, для дальнейшего возврата готовго тела для запроса
+    fun collectUserDataToPostBody(
+        inputTexts: List<String?>,
+        targetLanguage: String,
+        sourceLanguage: String
+    ): PostBody {
+        val targetLanguageCode = when (targetLanguage) {
+            RUSSIAN_INPUT_LANGUAGE -> RUSSIAN_LANGUAGE
+            ENGLISH_INPUT_LANGUAGE -> ENGLISH_LANGUAGE
+            else -> throw IllegalArgumentException("Illegal target language !")
+        }
+        val sourceLanguageCode = when (sourceLanguage) {
+            RUSSIAN_INPUT_LANGUAGE -> RUSSIAN_LANGUAGE
+            ENGLISH_INPUT_LANGUAGE -> ENGLISH_LANGUAGE
+            else -> throw IllegalArgumentException("Illegal source language !")
+        }
+        if (inputTexts.isNullOrEmpty() || inputTexts[0]?.trim() == "" || inputTexts[0] == null) {
+            throw IllegalArgumentException("Input text can't be null or empty !")
+        } else {
+            return PostBody(
+                texts = inputTexts as List<String>,
+                targetLanguageCode = targetLanguageCode,
+                sourceLanguageCode = sourceLanguageCode
+            )
+        }
+    }
+
+    companion object {
+        private const val RUSSIAN_LANGUAGE = "ru"
+        private const val ENGLISH_LANGUAGE = "en"
+        private const val RUSSIAN_INPUT_LANGUAGE = "Русский"
+        private const val ENGLISH_INPUT_LANGUAGE = "Английский"
+    }
 }
