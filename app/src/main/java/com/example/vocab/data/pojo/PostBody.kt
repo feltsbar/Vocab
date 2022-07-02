0af892de290f33d958db5e15c4b743157affa7a3
feltsbar <kuzmin.max6569@gmail.com>
2022-07-02 10:13:19 +0300

    clear code

--- app/src/main/java/com/example/vocab/data/pojo/PostBody.kt
+++ app/src/main/java/com/example/vocab/data/pojo/PostBody.kt
@@ -1,7 +1,7 @@
 package com.example.vocab.data.pojo
 
 data class PostBody(
-    val folderId: String = "b1gdti20dbhqdln0pmdo", //todo Вписать FOLDER_ID
+    val folderId: String = "", //todo Вписать FOLDER_ID
     val texts: List<String> = listOf("Hello"),
     val targetLanguageCode: String = "ru",
     val sourceLanguageCode: String = "en"
